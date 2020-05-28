f5b62802b3bed830e1b89f09e52a54c07e5e89cb
张雪飞 <zhangxf26@lenovo.com>
2020-05-28 18:53:41 +0800

    v1.0.1

---
+++ README.md
@@ -0,0 +1,10 @@
+# react-contextmenu-lite
+基于 React 实现的右键菜单，仅仅提供基本功能
+
+# Introduction
+包含2个高阶组件(ContextMenu, ContextMenuTrigger)
+
+# Example
+
+# Licence
+MIT
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'lib'),
-    filename: 'react-contextmenu.js',
+    filename: 'index.js',
     library: 'ReactContextMenu',
     libraryTarget: 'umd',
   },
