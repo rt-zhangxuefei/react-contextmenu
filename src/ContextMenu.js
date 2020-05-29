adaa81031f4ff34e72d594a17b8018c2cb3561b0
张雪飞 <zhangxf26@lenovo.com>
2020-05-29 15:26:59 +0800

    update

--- src/ContextMenu.js
+++ src/ContextMenu.js
@@ -1,35 +1,35 @@
-import React, { memo, useEffect } from "react";
-import "./ContextMenu.less";
+import React, { memo, useEffect } from 'react';
+import './ContextMenu.less';
 
 const ContextMenu = (props) => {
-  const { children, className = "", id, zIndex = 100 } = props;
+  const { children, className = '', id, zIndex = 100 } = props;
 
   useEffect(() => {
     const ctxMenu = document.getElementById(id);
-    window.addEventListener("click", () => {
+    window.addEventListener('click', () => {
       window.onwheel = null;
       if (ctxMenu) {
-        ctxMenu.style.visibility = "hidden";
+        ctxMenu.style.visibility = 'hidden';
       }
     });
     return () => {
-      window.removeEventListener("click", () => {
+      window.removeEventListener('click', () => {
         if (ctxMenu) {
-          ctxMenu.style.visibility = "hidden";
+          ctxMenu.style.visibility = 'hidden';
         }
       });
     };
   }, []);
 
   if (!id) {
-    return console.error("property id is required");
+    return console.error('property:id is required');
   }
 
   return (
     <div
       id={id}
       className={`context-menu ${className}`}
-      style={{ position: "fixed", zIndex, top: 0, left: 0, visibility: "hidden" }}
+      style={{ position: 'fixed', zIndex, top: 0, left: 0, visibility: 'hidden' }}
     >
       {children}
     </div>
--- src/ContextMenuTrigger.js
+++ src/ContextMenuTrigger.js
@@ -1,14 +1,14 @@
-import React, { memo, useCallback, useEffect } from "react";
+import React, { memo, useCallback, useEffect } from 'react';
 
 const ContextMenuTrigger = (props) => {
   const { children, id } = props;
 
   useEffect(() => {
-    window.addEventListener("contextmenu", (ev) => {
+    window.addEventListener('contextmenu', (ev) => {
       ev.preventDefault();
     });
     return () => {
-      window.removeEventListener("contextmenu", (ev) => {
+      window.removeEventListener('contextmenu', (ev) => {
         ev.preventDefault();
       });
     };
@@ -18,7 +18,7 @@ const ContextMenuTrigger = (props) => {
     const ctxtMenu = document.getElementById(id);
     let top = ev.clientY;
     let left = ev.clientX;
-    let { width, height } = ctxtMenu.getBoundingClientRect();
+    const { width, height } = ctxtMenu.getBoundingClientRect();
     const winWidth = window.innerWidth;
     const winHeight = window.innerHeight;
     if (winWidth - left < width) {
@@ -33,11 +33,11 @@ const ContextMenuTrigger = (props) => {
     };
     ctxtMenu.style.top = `${top}px`;
     ctxtMenu.style.left = `${left}px`;
-    ctxtMenu.style.visibility = "visible";
+    ctxtMenu.style.visibility = 'visible';
   }, []);
 
   if (!id) {
-    return console.error("property id is required");
+    return console.error('property id is required');
   }
   return <div onContextMenu={handleContextMenu}>{children}</div>;
 };
