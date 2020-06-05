b85972369bcbbd5e5bad7926100d2fd7b77811c1
张雪飞 <zhangxf26@lenovo.com>
2020-06-05 17:38:04 +0800

    update

--- src/ContextMenuTrigger.js
+++ src/ContextMenuTrigger.js
@@ -1,7 +1,8 @@
-import React, { memo, useCallback, useEffect } from 'react';
+import React, { memo, useCallback, useEffect, useRef } from 'react';
 
 const ContextMenuTrigger = (props) => {
   const { children, id } = props;
+  const timer = useRef(null);
 
   useEffect(() => {
     window.addEventListener('contextmenu', (ev) => {
@@ -16,24 +17,26 @@ const ContextMenuTrigger = (props) => {
   const handleContextMenu = useCallback((ev) => {
     ev.persist();
     const ctxtMenu = document.getElementById(id);
-    let top = ev.clientY;
-    let left = ev.clientX;
-    const { width, height } = ctxtMenu.getBoundingClientRect();
-    const winWidth = window.innerWidth;
-    const winHeight = window.innerHeight;
-    if (winWidth - left < width) {
-      left -= width;
-    }
-    if (winHeight - top < height) {
-      top -= height;
-    }
-
+    ctxtMenu.style.visibility = 'visible';
     window.onwheel = (ev2) => {
       ev2.preventDefault();
     };
-    ctxtMenu.style.top = `${top}px`;
-    ctxtMenu.style.left = `${left}px`;
-    ctxtMenu.style.visibility = 'visible';
+    let top = ev.clientY;
+    let left = ev.clientX;
+    clearTimeout(timer.current);
+    timer.current = window.setTimeout(() => {
+      const rect = ctxtMenu.getBoundingClientRect();
+      const winWidth = window.innerWidth;
+      const winHeight = window.innerHeight;
+      if (winWidth - left < rect.width) {
+        left -= rect.width;
+      }
+      if (winHeight - top < rect.height) {
+        top -= rect.height;
+      }
+      ctxtMenu.style.top = `${top}px`;
+      ctxtMenu.style.left = `${left}px`;
+    });
   }, []);
 
   if (!id) {
